e7b1353b69d6c772304f3ceb1a0a34918b1c58d3
unknown <zsda3131@naver.com>
2022-05-24 11:57:05 +0900

    finish community

--- finalpjt/src/views/ArticleListView.vue
+++ finalpjt/src/views/ArticleListView.vue
@@ -4,19 +4,22 @@
     <ul>
       <li v-for="article in articles" :key="article.pk">
         <!-- 작성자 -->
-        {{ article.category.name }}
-        {{ article.user.username }} : 
 
-        <!-- 글 이동 링크 (제목) -->
-      
-        <router-link 
-          :to="{ name: 'article', params: {articlePk: article.id} }">
-          {{ article.title }}
-        </router-link>
+          {{ article.category.name }}
+          {{ article.user.username }} : 
 
-        <!-- 댓글 개수/좋아요 개수 -->
-        =>
-        ({{ article.comment_count }}) | +{{ article.like_count }}
+          <!-- 글 이동 링크 (제목) -->
+        
+          <router-link 
+            :to="{ name: 'article', params: {articlePk: article.id} }">
+            {{ article.title }}
+          </router-link>
+
+          <!-- 댓글 개수/좋아요 개수 -->
+          =>
+          ({{ article.comment_count }}) | +{{ article.like_count }}
+
+
 
 
       </li>
@@ -24,20 +27,105 @@
         <router-link :to="{ name: 'articleNew'}">
           <button>글쓰러가기</button>
         </router-link>
+    <button @click="[categoryEnd(),toggle()]">결말포함</button>
+      <div v-if="show">
+        <div v-if="ends.length">
+          <ul>
+          <li v-for="(end,idx) in ends" :key="idx">
+            <!-- 작성자 -->
+
+              {{ end.category.name }}
+              {{ end.user.username }} : 
+
+              <!-- 글 이동 링크 (제목) -->
+            
+              <router-link 
+                :to="{ name: 'article', params: {articlePk: end.id} }">
+                {{ end.title }}
+              </router-link>
+
+              <!-- 댓글 개수/좋아요 개수 -->
+              =>
+              ({{ end.comment_count }}) | +{{ end.like_count }}
+
+
+
+
+          </li>
+        </ul>
+        </div>
+      </div>
+      <!--#############-->
+      <button @click="categoryWorld">세계관</button>
+      <div v-if="worlds.length">
+        <ul>
+        <li v-for="(world,idx) in worlds" :key="idx">
+          <!-- 작성자 -->
+
+            {{ world.category.name }}
+            {{ world.user.username }} : 
+
+            <!-- 글 이동 링크 (제목) -->
+          
+            <router-link 
+              :to="{ name: 'article', params: {articlePk: world.id} }">
+              {{ world.title }}
+            </router-link>
+
+            <!-- 댓글 개수/좋아요 개수 -->
+            =>
+            ({{ world.comment_count }}) | +{{ world.like_count }}
+
+
+
+
+        </li>
+      </ul>
+      </div>
    
   </div>
 </template>
 
 <script>
   import { mapActions, mapGetters } from 'vuex'
-
   export default {
     name: 'ArticleList',
+    data() {
+      return {
+        ends : [],
+        worlds : [],
+        eggs : [],
+        show: true
+      }
+
+    },
     computed: {
       ...mapGetters(['articles'])
     },
     methods: {
-      ...mapActions(['fetchArticles'])
+      ...mapActions(['fetchArticles']),
+      toggle: function(){
+        this.show =!this.show;
+      },
+      categoryEnd: function(){
+
+        // console.log(this.articles)
+        var arr = this.articles
+        const result = arr.filter(function(elem){
+          return elem.category.name === "결말포함"
+        });
+        this.ends = result
+      },
+      categoryWorld: function(){
+
+        // console.log(this.articles)
+        var arr = this.articles
+        const result = arr.filter(function(elem){
+          return elem.category.name === "세계관"
+        });
+        this.worlds = result
+      },
+
     },
     created() {
       this.fetchArticles()
